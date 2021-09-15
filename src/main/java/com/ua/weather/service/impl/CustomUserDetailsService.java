d81b36ea2b9ef340e6a07aa720248db9616e86c5
EFlatron <small.fin.murlock@gmail.com>
2021-09-16 01:04:18 +0300

    create user details model and service

---
+++ src/main/java/com/ua/weather/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.ua.weather.service.impl;
+
+import com.ua.weather.entity.User;
+import com.ua.weather.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+
+}
