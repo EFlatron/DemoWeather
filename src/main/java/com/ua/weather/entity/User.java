ed75474525c5a342708a75d26d892022d15dbd5f
EFlatron <small.fin.murlock@gmail.com>
2021-09-16 00:35:26 +0300

    create entity for user

---
+++ src/main/java/com/ua/weather/entity/User.java
@@ -0,0 +1,26 @@
+package com.ua.weather.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String firstName;
+
+    @Column(nullable = false, length = 20)
+    private String lastName;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+}
